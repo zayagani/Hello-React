0a0b684240246f10b12f48e2943b106c3a64bedb
zgani <zaya.gani@ariat.com>
2024-03-04 15:13:57 +0530

    add profile component

---
+++ Component/Profile/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Profile extends Component {
+  render() {
+    return (
+      <>
+        <div className="profileComponent">
+          <div className="profileSubComponent">Sub profile component</div>
+        </div>
+      </>
+    );
+  }
+}
