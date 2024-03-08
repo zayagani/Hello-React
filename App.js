ba944e791f3bc5bbb8138b7326169f15a876ad30
zgani <zaya.gani@ariat.com>
2024-03-08 15:55:55 +0530

    ignore node modules

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
-//import { Form } from ".";
+// import React from "react";
+// import Form from "./Component/form";
 
+//Createing HTML ELEMENT
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child" }, "sam", [
     React.createElement("h1", { id: "headings" }, "hello iam heading"),
@@ -11,17 +13,29 @@ const parent = React.createElement("div", { id: "parent" }, [
     React.createElement("h2", { id: "headings" }, "hello iam heading h2"),
   ]),
 
-  React.createElement("span", { id: "fewspan" }, "hello span"),
+  React.createElement(
+    "span",
+    { id: "fewspan", class: "fewspanClass", "data-id": "dataId" },
+    "hello span"
+  ),
+
+  React.createElement("button", { id: "btn" }, "Submit"),
+
+  React.createElement("p", { id: "paragraph" }, "create paragraph data"),
 ]);
 
-// const heading = React.createElement(
-//   "h1",
-//   { id: "heading", className: "headsup" },
-//   "hello react element"
-// );
+const heading = React.createElement(
+  "h1",
+  { id: "heading", className: "headsup" },
+  "hello react element"
+);
 
 console.log(parent, "ppp"); // heading is Object not h1 tag
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent); // render method is converting heading object into h1 tag
 
+// finding root where React element inject
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+//rendering that React element inside the root
+
+root.render(parent); // render method is converting heading object into h1 tag
 //export default App;
